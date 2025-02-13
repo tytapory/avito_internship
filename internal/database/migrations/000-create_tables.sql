e83aebd78be3144478ae1ca3f31a5ad1b6ce6b7f
Bezmaternih Vladislav Mikhailovich <BLA11111@yandex.ru>
2025-02-13 20:25:46 +0500

    WIP: sql migrations

---
+++ internal/database/migrations/000-create_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    balance INT DEFAULT 1000 CHECK (balance >= 0)
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL,
+    price INT NOT NULL CHECK (price >= 0)
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sender_id INT REFERENCES users(id),
+    receiver_id INT REFERENCES users(id),
+    amount INT NOT NULL CHECK (amount >= 0)
+);
+
+CREATE TABLE purchases (
+    id SERIAL PRIMARY KEY,
+    purchase_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    buyer_id INT REFERENCES users(id),
+    item_id INT REFERENCES items (id),
+    amount INT NOT NULL CHECK (amount >= 0)
+);
+
+CREATE TABLE user_items (
+    user_id INT REFERENCES users(id),
+    item_id INT REFERENCES items(id),
+    amount INT NOT NULL CHECK (amount >= 0),
+    PRIMARY KEY (user_id, item_id)
+);
