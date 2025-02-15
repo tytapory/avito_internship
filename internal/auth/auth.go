8e99c9eb58107f97f7432216ae335acc52b13cc0
Bezmaternih Vladislav Mikhailovich <BLA11111@yandex.ru>
2025-02-15 20:01:39 +0500

    Add authentication logic

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,93 @@
 package auth
+
+import (
+	"avito_internship/internal/config"
+	"avito_internship/internal/repository"
+	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+var ErrInvalidCredentials = errors.New("invalid credentials")
+var ErrExpiredToken = errors.New("expired token")
+
+// Authenticate выполняет вход или регистрирует пользователя.
+// Если пользователь найден, проверяет пароль и возвращает JWT если пароль верен.
+// Если пользователя нет, регистрирует его и выдает JWT.
+func Authenticate(username, password string) (string, error) {
+	if username == "" || password == "" || len(username) >= 32 {
+		return "", ErrInvalidCredentials
+	}
+	providedPassHash := getHash(password)
+	userID, passHash, err := repository.GetUserDataOrRegister(username, string(providedPassHash))
+	if err != nil {
+		return "", err
+	}
+	if isPasswordCorrect([]byte(password), passHash) {
+		return getJWT(userID), nil
+	} else {
+		return "", ErrInvalidCredentials
+	}
+}
+
+// VerifyJWT проверяет JWT и возвращает userID, если токен валиден.
+func VerifyJWT(tokenString string) (int, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return config.Get().JWTSecret, nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return 0, ErrInvalidCredentials
+	}
+
+	expFloat, ok := claims["exp"].(float64)
+	if !ok {
+		return 0, ErrExpiredToken
+	}
+	expiration := int64(expFloat)
+	if time.Now().Unix() > expiration {
+		return 0, ErrExpiredToken
+	}
+
+	userIDFloat, ok := claims["user_id"].(float64)
+	if !ok {
+		return 0, ErrInvalidCredentials
+	}
+	userID := int(userIDFloat)
+	return userID, nil
+}
+
+// getHash хэширует пароль с использованием bcrypt.
+func getHash(password string) []byte {
+	passHash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	return passHash
+}
+
+// isPasswordCorrect проверяет соответствие пароля и хэша.
+func isPasswordCorrect(providedPass, passHashFromDB []byte) bool {
+	err := bcrypt.CompareHashAndPassword(passHashFromDB, providedPass)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// getJWT создает JWT-токен для user_id со сроком действия 24 часа.
+func getJWT(userID int) string {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := token.SignedString(config.Get().JWTSecret)
+	return tokenString
+}
